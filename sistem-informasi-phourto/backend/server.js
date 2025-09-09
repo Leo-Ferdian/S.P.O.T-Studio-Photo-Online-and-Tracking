a7bede3a84c4a813dc56264ee31a1cf8f8e3b7ff
Leo Ferdiansyah <kaminarisan25@gmail.com>
2025-09-09 23:19:33 +0700

    Update server.js

--- sistem-informasi-phourto/backend/server.js
+++ sistem-informasi-phourto/backend/server.js
@@ -4,7 +4,8 @@ const cors = require('cors'); // Untuk mengizinkan request dari frontend
 
 // Impor rute
 const authRoutes = require('./src/api/routes/auth.routes');
-
+const authenticateToken = require('./src/api/middlewares/auth.middleware');
+const { registerValidationRules, loginValidationRules } = require('./src/api/validator/auth.validator');    
 const app = express();
 
 // Middleware
@@ -20,8 +21,31 @@ app.get('/', (req, res) => {
 // Gunakan Rute Autentikasi
 app.use('/api/auth', authRoutes);
 
+// Rute Protected (contoh)
+app.get('/api/protected', authenticateToken, (req, res) => {
+    res.json({
+        message: `Halo ${req.user.name}, endpoint ini terproteksi!`,
+        user: req.user
+    });
+});
+
 // Jalankan Server
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}`);
 });
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Terjadi kesalahan pada server!');
+});
+
+//cors configuration
+app.use(cors({
+  origin: 'http://localhost:3000', // nanti diubah ke domain frontend
+    credentials: true
+}));
+
+// Export app untuk testing atau penggunaan lainnya
+// module.exports = app;
