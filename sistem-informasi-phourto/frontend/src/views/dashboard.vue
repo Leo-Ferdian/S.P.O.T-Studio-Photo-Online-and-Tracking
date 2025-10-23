1d587ce6c810914248614f32e63839b710e431a3
Abel Hasti Ramadhani <abelhestiramadhani@gmail.com>
2025-10-23 20:52:01 +0700

    input service detail tes

--- sistem-informasi-phourto/frontend/src/views/dashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="pt-24 text-center">
-    <h1 class="text-3xl font-bold">Ini adalah Halaman Dashboard</h1>
-    <p class="text-gray-400">Hanya bisa diakses setelah login.</p>
-    </div>
-</template>
