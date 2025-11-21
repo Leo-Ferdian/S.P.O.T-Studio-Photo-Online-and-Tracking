dce879b48da24f80331f548a8ac1f108c3721e79
Leo Ferdiansyah <kaminarisan25@gmail.com>
2025-11-21 23:30:56 +0700

    Remove Contact Us route from the router configuration

--- sistem-informasi-phourto/frontend/src/router/index.js
+++ sistem-informasi-phourto/frontend/src/router/index.js
@@ -101,11 +101,6 @@ const routes = [
         component: () => import('../views/booking/Confirmation.vue'),
         meta: { requiresAuth: true },
     },
-    {
-        path: '/contact-us',
-        name: 'ContactUs',
-        component: () => import('../views/ContactUs.vue'),
-    },
     // {
     //     path: '/framecatalog',
     //     name: 'FrameCatalog',
