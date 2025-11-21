6558396ef46b4313a0aa1cd50d17f3635f0c03a9
abyanabdullah2549-code <abyan.abdullah2549@student.unri.ac.id>
2025-11-21 14:24:48 +0700

    update locationButton

--- sistem-informasi-phourto/frontend/src/components/common/LocationButton.vue
+++ sistem-informasi-phourto/frontend/src/components/common/LocationButton.vue
@@ -6,7 +6,7 @@
 
 <template>
     <button
-        class="w-full flex items-center space-x-4 bg-white text-text-default text-left p-4 border-3 border-outline shadow-solid hover:bg-slate-400 active:shadow-none active:translate-x-1 active:translate-y-1 transition-all duration-100">
+        class="w-full flex items-center space-x-4 bg-primary text-text-default text-left p-4 border-3 border-outline shadow-solid hover:bg-red-600 active:shadow-none active:translate-x-1 active:translate-y-1 transition-all duration-100">
         <i data-feather="map-pin" class="w-6 h-6"></i>
         <span class="font-bold">{{ location }}</span>
     </button>
